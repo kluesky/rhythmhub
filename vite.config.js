b1e711c94858f7641bdbae5e167552323c6f1272
Lyora <140289431+kluesky@users.noreply.github.com>
2026-04-07 01:53:06 +0700

    Fix formatting in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // Untuk akses dari jaringan lokal
+    host: true,
     port: 5173
   }
 })
