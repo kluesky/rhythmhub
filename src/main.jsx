ba60d818a8f1f69e6b0e11863927b3c504e20f9a
Lyora <140289431+kluesky@users.noreply.github.com>
2026-04-15 07:17:59 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { ToastProvider } from './context/ToastContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ToastProvider>
-      <App />
-    </ToastProvider>
+    <App />
   </React.StrictMode>,
 )
